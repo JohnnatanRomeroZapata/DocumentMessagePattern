3bc62a0a0bf118b5b9c5f6f594f04a3f52e78a8e
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-15 20:21:21 -0400

    project initialization

---
+++ src/main/java/com/example/DocumentMessagePattern/DocumentMessagePatternApplication.java
@@ -0,0 +1,12 @@
+package com.example.DocumentMessagePattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMessagePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentMessagePatternApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/DocumentMessagePattern/DocumentMessagePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocumentMessagePattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMessagePatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
